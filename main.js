f4685abab3a7e5a207867c6db17fdd163dfae5d9
laavang <laavang@gmail.com>
2020-01-15 20:51:41 -0600

    Built tic tac toe logic

--- main.js
+++ main.js
@@ -33,24 +33,88 @@ const printBoard = () => {
 }
 
 const horizontalWin = () => {
-  // Your code here to check for horizontal wins
+  if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') {
+    return true;
+  }
+  else if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O') {
+    return true;
+  }
+  else if (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') {
+    return true;
+  }
+  else if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O') {
+    return true;
+  }
+  else if (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') {
+    return true;
+  }
+  else if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O') {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 const verticalWin = () => {
-  // Your code here to check for vertical wins
+  if (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') {
+    return true;
+  }
+  else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'X') {
+    return true;
+  }
+  else if (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') {
+    return true;
+  }
+  else if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O') {
+    return true;
+  }
+  else if (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') {
+    return true;
+  }
+  else if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O') {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
+
 
 const diagonalWin = () => {
-  // Your code here to check for diagonal wins
+  if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+    return true;
+  }
+  else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+    return true;
+  }
+  else if (board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X') {
+    return true;
+  }
+  else if (board[2][0] == 'Y' && board[1][1] == 'Y' && board[0][2] == 'Y') {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
+
 const checkForWin = () => {
-  // Your code here call each of the check for types of wins
+  if (horizontalWin() == true || verticalWin() == true || diagonalWin() == true) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 const ticTacToe = (row, column) => {
-  // Your code here to place a marker on the board
-  // then check for a win
+  board[row][column] = playerTurn;
+  if (playerTurn == 'X') {
+    playerTurn = 'O'
+  }
+  checkForWin();
 }
 
 const getPrompt = () => {
